f6ffb3933c3b2da0cf26c10410f5098181d3a810
tianzhaotong2012 <tianzhaotong2012@github.com>
2018-02-06 11:56:40 +0800

    crop.sh change add crop image

--- crop.sh
+++ crop.sh
@@ -1,16 +1,35 @@
 #!bin/bash
-filelist=`ls /home/map/WebPageRecognition/img/conform/`
+baseDir=$(cd "$(dirname $0)"; pwd)
+dirList=`ls $baseDir/img/`
+for dirItem in $dirList
+do
+   echo $dirItem
+   echo "------"
+   if [ ! -d $baseDir/cropimg/$dirItem ];then
+	mkdir $baseDir/cropimg/$dirItem
+   fi
+   filelist=`ls $baseDir/img/$dirItem`
+   for file in $filelist
+   do
+	echo $file
+	convert $baseDir/img/$dirItem/$file -crop 1080x500 $baseDir/cropimg/$dirItem/$file
+   done
+done
+
+exit 1
+
+filelist=`ls $baseDir'/img/conform/'`
 for file in $filelist
 do
   echo $file
-  convert /home/map/WebPageRecognition/img/conform/$file -crop 1080x500 /home/map/WebPageRecognition/cropimg/conform/$file
+  convert $baseDir/img/conform/$file -crop 1080x500 $baseDir/cropimg/conform/$file
 done
 
 echo "--------"
 
-filelist1=`ls /home/map/WebPageRecognition/img/inconform/`
+filelist1=`ls $baseDir'/img/inconform/'`
 for file in $filelist1
 do
   echo $file
-  convert /home/map/WebPageRecognition/img/inconform/$file -crop 1080x500 /home/map/WebPageRecognition/cropimg/inconform/$file
+  convert $baseDir/img/inconform/$file -crop 1080x500 $baseDir/cropimg/inconform/$file
 done
